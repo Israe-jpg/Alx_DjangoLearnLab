2f589f7494cff09da3bb8c926537998027071f51
Israe-jpg <israeguennouni6@gmail.com>
2024-08-25 22:19:33 +0100

    hjk

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from .models import Book
 
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+# Login View - using Django's built-in view
+class LoginView(auth_views.LoginView):
+    template_name = 'login.html'
+
+# Logout View - using Django's built-in view
+class LogoutView(auth_views.LogoutView):
+    template_name = 'logout.html'
+
+# Registration View - using a custom view based on Django's UserCreationForm
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
