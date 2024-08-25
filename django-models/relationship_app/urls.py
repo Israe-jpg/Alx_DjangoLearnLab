b7b0ad83ebf5c38af602474034aecf6399088588
Israe-jpg <israeguennouni6@gmail.com>
2024-08-25 22:27:12 +0100

    hjk

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,20 +1,14 @@
 from django.urls import path
 from . import views
 from .views import list_books
-
 from .views import LoginView, LogoutView, RegisterView
-
-
-
-urlpatterns = [
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('register/', register, name='register'),
-]
-
 
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+from django.views.generic import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
 
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth import login
-
-
 
 # Login View - using Django's built-in view
 class LoginView(auth_views.LoginView):
@@ -19,22 +29,8 @@ class LogoutView(auth_views.LogoutView):
 
 # Registration View - using a custom view based on Django's UserCreationForm
 def register(request):
-    form = UserCreationForm(request.POST or None)
+    form = UserCreationForm()", "relationship_app/register.html
     if form.is_valid():
         form.save()
         return redirect('login')
     return render(request, 'register.html', {'form': form})
-
-
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-from django.views.generic import DetailView
-from django.views.generic.detail import DetailView
-from .models import Library
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
