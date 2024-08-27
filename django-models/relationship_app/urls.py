7cc54921864c667f0088c1209b03736172696d2b
Israe-jpg <israeguennouni6@gmail.com>
2024-08-27 16:04:26 +0100

    hjk

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import add_book, edit_book, delete_book
+from . import views
+from .views import list_books
+from .views import LoginView, LogoutView, RegisterView
+
 
 urlpatterns = [
-    path('add/', add_book, name='add_book'),
-    path('edit/<int:pk>/', edit_book, name='edit_book'),
-    path('delete/<int:pk>/', delete_book, name='delete_book'),
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    
 ]
-add_book/", "edit_book/
