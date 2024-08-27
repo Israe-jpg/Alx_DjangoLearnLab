180e132f2c88fa133ec806aac2ddf79940ba1101
Israe-jpg <israeguennouni6@gmail.com>
2024-08-27 15:59:43 +0100

    hjk

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,16 +1,32 @@
 from django.db import models
 
+# Author Model
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def _str_(self):
+        return self.name
+
+# Book Model
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
-    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
-    def __str__(self):
+    def _str_(self):
         return self.title
 
-    class Meta:
-        permissions = [
-            ('can_add_book', 'Can add book'),
-            ('can_change_book', 'Can change book'),
-            ('can_delete_book', 'Can delete book'),
-        ]
+# Library Model
+class Library(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def _str_(self):
+        return self.name
+
+# Librarian Model
+class Librarian(models.Model):
+    name = models.CharField(max_length=255)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
+    def _str_(self):
+        return self.name
